bb0b971a31975b1517498630ba3deee8afc0cdfa
Ann <ann.chu.creator@gmail.com>
2025-02-16 04:49:08 +0800

    Fix zoom-in

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-// Back to top function
-window.onscroll = function() {
-    let backToTop = document.getElementById("backToTop");
-    if (document.documentElement.scrollTop > 200) {
-        backToTop.style.display = "block";
-    } else {
-        backToTop.style.display = "none";
-    }
-};
-
 // Smooth Scroll to Top
 document.getElementById("backToTop").addEventListener("click", function() {
     window.scrollTo({ top: 0, behavior: "smooth" });
